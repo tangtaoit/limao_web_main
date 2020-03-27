743e1b0dbf7fdeddb9b1fddcf78660ce6916b25d
tangtao <tt@qihyunxin.com>
2020-03-27 16:31:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="onTest">{{uid}}</button>
+    <iframe
+      ref="chat"
+      :src="imURL"
+      style="width:100%;height:100vh"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import lim from "limao";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    const uid = "555539517403037720"
+    const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyLXJlc291cmNlIl0sInVzZXJfbmFtZSI6ImhlaG9uZ2JpbmciLCJzY29wZSI6WyJhbGwiXSwiZXhwIjoxNTg1MzAwMjIxLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiNjBjZWUyNmMtZTBkNy00ODgyLTliNTItNmIxODIyYmY5NGUxIiwiY2xpZW50X2lkIjoiY29jbyJ9.xhPaaqqb3rFeLIJoUXCacVDm9OhCG4Dsy-irvCS1mpw"
+     return {
+       uid: uid,
+       token: token,
+       imURL: `http://localhost:3000?uid=${uid}&token=${token}&channel_id=636595900868722688&channel_type=1&apiURL=https://appuat.cocospace.com.cn/api/`
+     }
+  },
+  mounted() {
+    lim.options.connectURL = "ws://116.62.53.36:8030/"
+    // 监听连接状态
+    lim.addConnectStatusListener((status,reasonCode)=>{
+      console.log('status->',status,reasonCode);
+      if(status == 1) {
+        console.log('连接成功！');
+      }else {
+        console.log('连接失败！');
+      }
+    });
+    // IM消息状态改变
+    lim.addMessageStatusListener((sendackPacket)=>{
+         const limWindow = this.$refs.chat.contentWindow;
+         let newSendackPacket = {...sendackPacket}
+         newSendackPacket.messageID = sendackPacket.messageID.toString() // 消息编号是bignumber类型 需要特殊处理下
+         limWindow.postMessage({type:"sendack",data:newSendackPacket},"*");
+    });
+     // 监听消息
+    lim.addMessageListener((message)=>{
+        console.log('收到消息->',message);
+        let newMessage = {...message}
+        const limWindow = this.$refs.chat.contentWindow;
+        newMessage.messageID = message.messageID.toString();
+        limWindow.postMessage({type:"recv",data:newMessage},"*");
+    });
+
+    // 监听子窗口发过来的请求
+    window.addEventListener("message",(event)=>{
+       if(!event.data) {
+         return;
+       }
+       console.log('event->',event);
+       let type = event.data.type;
+       if(type === "send") { // 发送消息
+          lim.sendMessage(event.data.payload,event.data.channel);
+       }
+    }) 
+    // 连接IM
+    console.log('this.uid--->',this.uid);
+    console.log('this.token--->',this.token);
+    lim.connect(this.uid,this.token);
+
+  },
+  components: {},
+  methods: {
+    onTest() {
+    // this.$refs.chat.contentWindow.lim11 = "dsdsdszzz";
+    console.log('this.$refs.chat.contentWindow->',this.$refs.chat.contentWindow);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -23,6 +83,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
 }
 </style>
